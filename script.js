5180a7222ec2bafc0dba2bb8d473031db33dd883
Henrique Cestaro <hjcestaro@hotmail.com>
2020-11-23 15:31:47 -0300

    Primeiro Commmit

---
+++ script.js
@@ -0,0 +1,31 @@
+const cat_result = document.getElementById('cat_result')
+const dog_result = document.getElementById('dog_result')
+
+const gat_btn = document.getElementById('gat_btn')
+const dog_btn = document.getElementById('dog_btn')
+
+
+gat_btn.addEventListener('click', getRandomGat)
+
+dog_btn.addEventListener('click', getRandomDog)
+
+
+function getRandomGat() {
+    fetch('https://aws.random.cat/meow')
+        .then(res => res.json())
+        .then(data => {
+            cat_result.innerHTML = `<img src=${data.file} />`
+        })
+}
+
+function getRandomDog() {
+    fetch('https://random.dog/woof.json')
+        .then(res => res.json())
+        .then(data => {
+            if (data.url.includes('.mp4')) {
+                getRandomDog()
+            } else {
+                dog_result.innerHTML = `<img src=${data.url} />`
+            }
+        })
+} 
